fdbbc8fe92327e59065022f761df9db9762380bd
Bruno Campos <brunogalvaocampos#gmail.com>
2021-07-02 18:26:36 -0300

    edit and delete routes implemented

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,7 @@ const Campground = require("../models/campground");
 const cities = require("./cities")
 const {places, descriptors} = require("./seedHelpers");
 
+// Connecting to the database
 mongoose.connect("mongodb://localhost:27017/Campground-probe", {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -17,17 +18,17 @@ db.once("open", () => {
     console.log("database connected")
 });
 
-// pick a random element of an array
+// pick a random element of an array (to randomly generate the camping names)
 const sample = array => array[Math.floor(Math.random() * array.length)]
 
-
+// seed our database
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let index = 0; index < 50; index++) {
-        const random66 = Math.floor(Math.random() * 66); //we have 66 cities
+    for (let index = 0; index < 50; index++) { //create 50 camping sites
+        const random66 = Math.floor(Math.random() * 66); //pick the 66 cities randomly
         const camp = new Campground({
             location: `${cities[random66].nome}`,
-            title: `Camping ${sample(places)} ${sample(descriptors)}`
+            title: `${sample(places)} ${sample(descriptors)}`
         })
         await camp.save();
     }
