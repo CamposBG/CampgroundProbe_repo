2e9e636a10dc8686023160f584c61cab5c655349
Bruno Campos <brunogalvaocampos#gmail.com>
2021-07-06 12:46:50 -0300

    add basic style at index and eddit and new

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 // Making the Schema
 const CampgroundSchema = new Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String,
 });
