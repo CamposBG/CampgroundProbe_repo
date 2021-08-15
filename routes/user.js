4256423d6e5f82b7e5bc4389f4ba3c1df22853b1
CamposBG <brunogalvaocampos@gmail.com>
2021-08-15 14:33:09 -0300

    Add route to create new user - register

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/register", (req, res) =>{
+router.get("/register", (req, res) => {
     res.render("users/register")
 })
 
-router.post("/register", async(req, res) =>{
-    res.send(req.body);
+router.post("/register", async (req, res) => {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password)
+    console.log(registeredUser)
+    req.flash("success", "Welcome to Campground Probe!");
+    res.redirect("/campgrounds");
 })
 
 module.exports = router;
