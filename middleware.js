ea6adc9b9cf4c1221233153ec26b3e2c57354696
CamposBG <brunogalvaocampos@gmail.com>
2021-08-16 17:37:49 -0300

    Add redirect usr back to the last url after login

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) { //isAuthenticated is a method of passport that check if the user is signed in 
+        req.session.returnTo = req.originalUrl //store the url to redirect someone after login or register
         req.flash("error", "you must be signed in first")
         return res.redirect("/login")
     }
--- routes/user.js
+++ routes/user.js
@@ -34,7 +34,9 @@ router.post("/login",
     passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }),
     (req, res) => {
         req.flash("success", "Welcome back!");
-        res.redirect("/campgrounds");
+        const redirectUrl = req.session.returnTo || "/campgrounds"
+        delete req.session.returnTo;
+        res.redirect(redirectUrl);
     }
 )
 
