b0e065a21fba6437e351faa41fd5610ed0a2ba1f
CamposBG <brunogalvaocampos@gmail.com>
2021-08-16 17:21:05 -0300

    Add login logout and register interactive btns

--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,23 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync")
 const User = require("../models/user");
 const passport = require("passport");
+const { isLoggedIn } = require("../middleware");
 
 router.get("/register", (req, res) => {
     res.render("users/register")
 })
 
-router.post("/register", catchAsync(async (req, res) => {
+router.post("/register", catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password)
-        req.flash("success", "Welcome to Campground Probe!");
-        res.redirect("/campgrounds");
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash("success", "Welcome to Campground Probe!");
+            res.redirect("/campgrounds");
+        })
+
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/register")
@@ -34,7 +39,7 @@ router.post("/login",
 )
 
 //logout route
-router.get("/logout", (req, res)=>{
+router.get("/logout", (req, res) => {
     req.logOut();
     req.flash("success", "Logged out!")
     res.redirect("/campgrounds")
