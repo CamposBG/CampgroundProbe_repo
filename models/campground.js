17872213fcffb0e0c4f3fd411ae18525249fdf64
CamposBG <brunogalvaocampos@gmail.com>
2021-08-23 16:42:53 -0300

    Add Clusters on the map

--- models/campground.js
+++ models/campground.js
@@ -1,3 +1,4 @@
+const { options } = require('joi');
 const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
@@ -10,6 +11,9 @@ const ImageSchema = new Schema({
 ImageSchema.virtual("thumbnail").get(function () {
     return this.url.replace("/upload", "/upload/w_200");
 });
+
+const opts = { toJSON: { virtuals: true } }
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -20,7 +24,7 @@ const CampgroundSchema = new Schema({
             required: true
         },
         coordinates: {
-            type:[Number],
+            type: [Number],
             required: true
         }
     },
@@ -37,6 +41,11 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return ` <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0,20)}...</p>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -21,15 +21,22 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 100; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: "611954449dc960481a7f6d70",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
             price,
-            description: 'texto modelo para todos os casos',
+            geometry:{
+                type:"Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [{ 
                 url: "https://res.cloudinary.com/dwal0ydpw/image/upload/v1629324989/YelpCamp/d55osraemp7wdroqh4rh.jpg",
                 filename: "YelpCamp/d55osraemp7wdroqh4rh" 
