068cf8f025550ced2280a90fc28b0669f58a5c2a
CamposBG <brunogalvaocampos@gmail.com>
2021-08-24 14:35:49 -0300

    Add style to home, login and register form

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,4 +1,3 @@
-
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -6,7 +5,7 @@ const map = new mapboxgl.Map({
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
-
+map.addControl(new mapboxgl.NavigationControl());
 // Create a default Marker and add it to the map.
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
